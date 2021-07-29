348646ba8d86e2a8225d49040a042ed8672fd26e
oscar bernardini <bernardini687@gmail.com>
2021-07-29 16:57:40 +0200

    test calc month

--- kakebo/kakebo_test.go
+++ kakebo/kakebo_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+// Test CalcBalance
+//
 func TestCalcBalance(t *testing.T) {
 	data := `-120 y foo
 -34.5 m bar
@@ -20,7 +22,23 @@ func TestCalcBalance(t *testing.T) {
 	}
 }
 
-func TestCalcBalanceEmpty(t *testing.T) {
+func TestCalcBalanceFieldsErr(t *testing.T) {
+	data := `-120 y foo
+-34.5
+-6 M baz
+`
+	want := "at least 2 fields required"
+
+	got, err := CalcBalance(data)
+	message := err.Error()
+	balance := got.StringFixed(2)
+
+	if balance != "0.00" || message != want {
+		t.Fatalf("\nGOT: %#v, `%v`\nWANT: \"0.00\", `%v`", balance, message, want)
+	}
+}
+
+func TestCalcBalanceEmptyErr(t *testing.T) {
 	data := ""
 	want := "at least 2 fields required"
 
@@ -33,7 +51,23 @@ func TestCalcBalanceEmpty(t *testing.T) {
 	}
 }
 
-func TestCalcBalanceInterval(t *testing.T) {
+func TestCalcBalanceAmountErr(t *testing.T) {
+	data := `-120 y foo
+-34,5 m bar
+-6 M baz
+`
+	want := "can't convert -34,5 to decimal"
+
+	got, err := CalcBalance(data)
+	message := err.Error()
+	balance := got.StringFixed(2)
+
+	if balance != "0.00" || message != want {
+		t.Fatalf("\nGOT: %#v, `%v`\nWANT: \"0.00\", `%v`", balance, message, want)
+	}
+}
+
+func TestCalcBalanceIntervalErr(t *testing.T) {
 	data := `-120 y foo
 -34.5 X bar
 -6 M baz
@@ -48,3 +82,71 @@ func TestCalcBalanceInterval(t *testing.T) {
 		t.Fatalf("\nGOT: %#v, `%v`\nWANT: \"0.00\", `%v`", balance, message, want)
 	}
 }
+
+// Test CalcBalance
+//
+func TestCalcMonth(t *testing.T) {
+	data := `1.2 foo
+3.45 bar
+6 baz
+78.09 xyzzy
+`
+	want := "88.74"
+
+	got, err := CalcMonth(data)
+	balance := got.StringFixed(2)
+
+	if balance != want || err != nil {
+		t.Fatalf("\nGOT: %#v, `%v`\nWANT: %#v, `<nil>`", balance, err, want)
+	}
+}
+
+func TestCalcMonthFieldsErr(t *testing.T) {
+	data := `1.2 foo
+
+6 baz
+78.09 xyzzy
+`
+	want := "at least 1 field required"
+
+	got, err := CalcMonth(data)
+	message := err.Error()
+	balance := got.StringFixed(2)
+
+	if balance != "0.00" || message != want {
+		t.Fatalf("\nGOT: %#v, `%v`\nWANT: \"0.00\", `%v`", balance, message, want)
+	}
+}
+
+func TestCalcMonthEmptyErr(t *testing.T) {
+	data := ""
+	want := "at least 1 field required"
+
+	got, err := CalcMonth(data)
+	message := err.Error()
+	balance := got.StringFixed(2)
+
+	if balance != "0.00" || message != want {
+		t.Fatalf("\nGOT: %#v, `%v`\nWANT: \"0.00\", `%v`", balance, message, want)
+	}
+}
+
+func TestCalcMonthAmountErr(t *testing.T) {
+	data := `1.2 foo
+bar
+6 baz
+78.09 xyzzy
+`
+	want := "can't convert bar to decimal"
+
+	got, err := CalcMonth(data)
+	message := err.Error()
+	balance := got.StringFixed(2)
+
+	if balance != "0.00" || message != want {
+		t.Fatalf("\nGOT: %#v, `%v`\nWANT: \"0.00\", `%v`", balance, message, want)
+	}
+}
+
+// Test DisplayMonth
+//
